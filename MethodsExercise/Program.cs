608f5adade592d2fe6360ad82cce6e7fa23fdf71
Eric Caro <c.aroericm@gmail.com>
2023-06-18 23:29:26 -0500

    Exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -9,6 +9,15 @@ namespace MethodsExercise
             Console.WriteLine("Hey you");
             Console.ReadLine();
 
+            Console.WriteLine("What are you doing?");
+            Console.ReadLine();
+
+            Console.WriteLine("That's what's up. I am bored. It gets lonely being stuck inside a computer ya know?");
+            Console.ReadLine();
+
+            Console.WriteLine("But did you have a good weekend?");
+            Console.ReadLine();
+
             Console.WriteLine("What is your name?");
             Console.Write("Name: ");
             var name = Console.ReadLine();
@@ -23,8 +32,25 @@ namespace MethodsExercise
             
             Console.WriteLine("What is your favorite band?");
             Console.Write("Band: ");
-            var band = Console.ReadLine(); 
-            
+            var band = Console.ReadLine();
+
+            Console.WriteLine("Can I ask you one more question?");
+            Console.ReadLine();
+
+            Console.WriteLine("Who's your favorite basketball player? I am trying to see something");
+            Console.ReadLine();
+
+            Console.WriteLine("Oh ok");
+            Console.ReadLine();
+
+            Console.WriteLine("Do you want to know who my favorite basketball player of all time is?");
+            Console.ReadLine();
+
+            Console.WriteLine("Oh ok. Well I am going to tell your regardless. It's Michael Jordan because he could score 63 points on a Larry Bird Celtics team EVEN when being fouled and triple teamed the entire series. That alone makes him the greatest");
+            Console.ReadLine();
+
+            Console.WriteLine("Anyways, nice chatting with you!");
+            Console.ReadLine();
 
 
 
