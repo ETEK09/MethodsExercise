9fe4c5d1637ae59dfa074faf77f10e7903b092ec
Eric Caro <c.aroericm@gmail.com>
2023-06-19 14:23:24 -0500

    Exercise 1 and 2)

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise;
+
+public class Methods
+
+{
+    public static int Add(int num1, int num2)
+    {
+        int sum = num1 + num2;
+
+        return sum;
+    }
+
+    public static int Subtract(int num1, int num2)
+    {
+        int sum = num1 - num2;
+        return sum;
+
+    }
+
+    public static int muliply(int num1, int num2)
+    {
+        int sum = num1 * num2;
+        return sum;
+
+    }
+
+    public static int divide(int num1, int num2)
+    {
+        int sum = num1 / num2;
+        return sum;
+
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -8,59 +8,67 @@ namespace MethodsExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hey you");
-            Console.ReadLine();
+            //        Console.WriteLine("Hey you");
+            //        Console.ReadLine();
 
-            Console.WriteLine("What are you doing?");
-            Console.ReadLine();
+            //        Console.WriteLine("What are you doing?");
+            //        Console.ReadLine();
 
-            Console.WriteLine("That's what's up. I am bored. It gets lonely being stuck inside a computer ya know?");
-            Console.ReadLine();
+            //        Console.WriteLine("That's what's up. I am bored. It gets lonely being stuck inside a computer ya know?");
+            //        Console.ReadLine();
 
-            Console.WriteLine("But did you have a good weekend?");
-            Console.ReadLine();
+            //        Console.WriteLine("But did you have a good weekend?");
+            //        Console.ReadLine();
 
-            Console.WriteLine("What is your name?");
-            Console.Write("Name: ");
-            var name = Console.ReadLine();
+            //        Console.WriteLine("What is your name?");
+            //        Console.Write("Name: ");
+            //        var name = Console.ReadLine();
 
-            Console.WriteLine($"So " + name + ". Now that I have your here, what is your favorite color?");
-            Console.Write("Color: ");
-            var color = Console.ReadLine();
+            //        Console.WriteLine("What is your favorite color?");
+            //        Console.WriteLine($"So " + name + ". Now that I have your here, what is your favorite color?");
+            //        Console.Write("Color: ");
+            //        var color = Console.ReadLine();
 
-            Console.WriteLine(color + "? Wow that's a nice color. What is your favorite animal?");
-            Console.Write("Animal: ");
-            var animal = Console.ReadLine();
+            //        Console.WriteLine("What is your favorite animal?");
+            //        Console.WriteLine(color + "? Wow that's a nice color. What is your favorite animal?");
+            //        Console.Write("Animal: ");
+            //        var animal = Console.ReadLine();
 
-            Console.WriteLine($"A " + animal + " is a good choice indeed. Who is your favorite band?");
-            Console.Write("Band: ");
-            var band = Console.ReadLine();
+            //        Console.WriteLine($"A " + animal + " is a good choice indeed. Who is your favorite band?");
+            //        Console.Write("Band: ");
+            //        var band = Console.ReadLine();
 
-            Console.WriteLine($"Nice! " + band + " has some smooth classics for sure.");
-            Console.ReadLine();
+            //        Console.WriteLine("Can I ask you one more question?");
+            //        Console.WriteLine($"Nice! " + band + " has some smooth classics for sure.");
+            //        Console.ReadLine();
 
+            //        Console.WriteLine("Who's your favorite basketball player? I am trying to see something");
 
-            Console.WriteLine($"Can I ask you one more question?");
-            Console.ReadLine();
+            //        Console.WriteLine($"Can I ask you one more question?");
+            //        Console.ReadLine();
 
-            Console.WriteLine("Who's your favorite basketball player? I am trying to see something");
-            Console.Write("Answer: ");
-            var answer = Console.ReadLine();
+            //        Console.WriteLine("Oh ok");
+            //        Console.WriteLine("Who's your favorite basketball player? I am trying to see something");
+            //        Console.Write("Answer: ");
+            //        var answer = Console.ReadLine();
 
-            Console.WriteLine($"Oh ok. " + answer + " is a good pick!");
-            Console.ReadLine();
+            //        Console.WriteLine($"Oh ok. " + answer + " is a good pick!");
+            //        Console.ReadLine();
 
-            Console.WriteLine("Do you want to know who my favorite basketball player of all time is?");
-            Console.ReadLine();
+            //        Console.WriteLine("Do you want to know who my favorite basketball player of all time is?");
+            //        Console.ReadLine();
 
-            Console.WriteLine("Oh ok. Well I am going to tell your regardless. It's Michael Jordan because he could score 63 points on a Larry Bird Celtics team EVEN when being fouled and triple teamed the entire series. That alone makes him the greatest");
-            Console.ReadLine();
+            //        Console.WriteLine("Oh ok. Well I am going to tell your regardless. It's Michael Jordan because he could score 63 points on a Larry Bird Celtics team EVEN when being fouled and triple teamed the entire series. That alone makes him the greatest");
+            //        Console.ReadLine();
 
-            Console.WriteLine("Anyways, nice chatting with you!");
-            Console.ReadLine();
+            //        Console.WriteLine("Anyways, nice chatting with you!");
+            //        Console.ReadLine();
 
+            Console.WriteLine(Methods.divide(43, 21));
 
 
         }
+
+
     }
 }
