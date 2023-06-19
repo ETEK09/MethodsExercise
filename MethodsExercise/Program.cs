380bdb785d9cf1a565209b9c253fd5a412205a7c
Eric Caro <c.aroericm@gmail.com>
2023-06-18 23:13:33 -0500

    console.write

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hey you");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            Console.Write("Color: ");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            Console.Write("Animal: ");
+            var animal = Console.ReadLine(); 
+            
+            Console.WriteLine("What is your favorite band?");
+            Console.Write("Band: ");
+            var band = Console.ReadLine(); 
+            
+
+
+
+
+
+
+
+
         }
     }
 }
